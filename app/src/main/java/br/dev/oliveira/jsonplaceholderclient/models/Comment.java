4f493db8d6de9124129f53c386fabef7d8666c5f
Leandro Oliveira <oliveira.dev1997@gmail.com>
2018-02-04 19:13:11 -0200

    Inserção da model Comment

---
+++ app/src/main/java/br/dev/oliveira/jsonplaceholderclient/models/Comment.java
@@ -0,0 +1,47 @@
+package br.dev.oliveira.jsonplaceholderclient.models;
+
+public class Comment {
+
+    private Integer id, postId;
+    private String  name, email, body;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
